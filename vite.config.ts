0f91ed08c754fd8996af73d8ef0e226f37707058
Samuel Gamito <samuelgamito96@gmail.com>
2026-03-27 21:39:46 -0300

    add nginx and build pipe

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
+  },
+  preview: {
+    host: "::",
+    port: 4173,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
